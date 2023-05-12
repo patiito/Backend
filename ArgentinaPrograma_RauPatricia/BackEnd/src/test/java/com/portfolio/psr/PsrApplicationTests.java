e0a016abc30c9f23b67493e4a1268c86b1bb223c
patiito <118027280+patiito@users.noreply.github.com>
2023-05-12 16:30:01 -0300

    backend

---
+++ ArgentinaPrograma_RauPatricia/BackEnd/src/test/java/com/portfolio/psr/PsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.psr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
